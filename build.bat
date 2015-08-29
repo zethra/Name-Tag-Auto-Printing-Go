e68b08c64ab3252d10f956431bf8c5d43cc5151c
zethra <jediben97@gmail.com>
2015-08-29 01:56:56 -0400

    fix

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
 set GOPATH=%cd%
+go get github.com/kardianos/osext
+go get github.com/satori/go.uuid
 go build -o %GOPATH%\bin\NameTagAutoPrinting.exe %GOPATH%\src\main.go
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export GOPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+go get github.com/kardianos/osext
+go get github.com/satori/go.uuid
 go build -o $GOPATH/bin/NameTagAutoPrinting.exe $GOPATH/src/main.go
